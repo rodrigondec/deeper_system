bcab5b9255b4fb3c828aa8884f4db9f0f343220e
rodrigondec <rodrigondec@gmail.com>
2019-06-28 00:54:08 -0300

    Video & Theme models

--- app/models.py
+++ app/models.py
@@ -1,15 +1,23 @@
-from mongoengine import connect, Document, StringField, IntField
+from mongoengine import connect, Document, StringField, IntField, ReferenceField, ListField, NULLIFY
 from decouple import config
 
 
-connect(config('DB_NAME'), host=config('DB_HOST'), port=config('DB_PORT'))
+connect(
+    db=config('DB_NAME'),
+    username=config('DB_USER'),
+    password=config('DB_PASSWORD'),
+    host=config('DB_HOST'),
+    port=config('DB_PORT', cast=int)
+)
+
+
+class Theme(Document):
+    name = StringField()
+    videos = ListField(ReferenceField("Video"))
 
 
 class Video(Document):
     name = StringField()
     thumbs_up = IntField()
     thumbs_down = IntField()
-
-
-class Theme(Document):
-    name = StringField()
+    theme = ReferenceField(Theme, reverse_delete_rule=NULLIFY)
