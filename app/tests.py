eeff3bd0de7ba7ad8fb051e036fe7c3068e9df36
rodrigondec <rodrigondec@gmail.com>
2019-06-27 23:07:41 -0300

    routes ok

--- app/tests.py
+++ app/tests.py
@@ -11,10 +11,17 @@ class ViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_my_view(self):
-        from .views.default import my_view
+        from .views.default import home_view
         request = testing.DummyRequest()
-        info = my_view(request)
+        info = home_view(request)
         self.assertEqual(info['project'], 'deeper_system')
+
+    def test_howdy(self):
+        from .views.howdy import howdy_view
+        request = testing.DummyRequest()
+        response = howdy_view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Howdy!!', response.body)
 
 
 class FunctionalTests(unittest.TestCase):
@@ -27,3 +34,7 @@ class FunctionalTests(unittest.TestCase):
     def test_root(self):
         res = self.testapp.get('/', status=200)
         self.assertTrue(b'Pyramid' in res.body)
+
+    def test_howdy(self):
+        res = self.testapp.get('/howdy', status=200)
+        self.assertTrue(b'Howdy!!' in res.body)
--- app/views/default.py
+++ app/views/default.py
@@ -2,5 +2,5 @@ from pyramid.view import view_config
 
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
+def home_view(request):
     return {'project': 'deeper_system'}
